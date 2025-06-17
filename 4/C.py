39838ab2d115977ee8f5428fb644beb2450390d7
MrAndrew30 <andrey@MacBook-Air-Andrey-2.local>
2025-06-17 20:13:36 +0300

    Fixed ML: 4 contest - C. Самое частое слово

--- 4/C.py
+++ 4/C.py
@@ -1,11 +1,9 @@
 import sys
 
-text = " ".join(i.replace("\n", "") for i in sys.stdin)
 d = dict()
-
-for word in text.split():
-    if word not in d:
-        d[word] = 0
-    d[word] += 1
-items = sorted(d.items(), key=lambda x: (-x[1], x[0]))
-print(items[0][0])
+for text in sys.stdin:
+    for word in text.split():
+        if word not in d:
+            d[word] = 0
+        d[word] += 1
+print(min(d.items(), key=lambda x: (-x[1], x[0]))[0])
